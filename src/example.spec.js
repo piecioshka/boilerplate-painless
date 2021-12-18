732d737067419ed5c893e5eb4e4f178ac0e0b582
Piotr Kowalski <piecioshka@gmail.com>
2021-12-18 14:10:28 +0100

    Initialize project

---
+++ src/example.spec.js
@@ -0,0 +1,34 @@
+const {
+  createGroup,
+  assert,
+  spy,
+  stub,
+  mock,
+  sinon,
+  chai,
+} = require("painless");
+import { Example } from "../src/example";
+
+const test = createGroup();
+
+let o;
+
+test.beforeEach(() => {
+  o = new Example();
+});
+
+test("sync", () => {
+  assert.equal(o.sync(), "sync");
+});
+
+test("asyncCallback", (done) => {
+  o.asyncCallback((value) => {
+    assert.equal(value, "asyncCallback");
+    done();
+  });
+});
+
+test("asyncPromise", async () => {
+  const value = await o.asyncPromise();
+  assert.equal(value, "asyncPromise");
+});
